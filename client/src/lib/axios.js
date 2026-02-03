add98b2360f89ef47e73b4e80fc8bf50dc145291
trinhHuyLong <trinhhuylong9304@gmail.com>
2026-02-03 23:24:15 +0700

    commit

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
+      ? "https://hlchat-jw9f.onrender.com"
       : "/api",
   withCredentials: true,
 });
