7acdeea334e2f1110503e5cbaea4dbfa43e4c84a
trinhHuyLong <trinhhuylong9304@gmail.com>
2026-02-04 00:38:19 +0700

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://hlchat.vercel.app/"],
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
