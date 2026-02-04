ce6f0344cab360ea4d9d3c658f3cd1bfe729c32d
trinhHuyLong <113961733+trinhHuyLong@users.noreply.github.com>
2026-02-04 20:39:03 +0700

    0

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://hlchat.vercel.app/"],
+    origin: [ "https://hlchat.vercel.app"],
   },
 });
 
